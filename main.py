3f63b0acfb16924f9e06f88199d3b04dffd0e302
James Huang <jhuang2003@yahoo.ca>
2022-01-06 22:33:41 -0800

    implemented voice recognition with PyAudio, Speech Recognition, and pyttsx3

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-# main
-
-
+# main.py
 # import modules for voice and pyautogui
 import commands
 import time
 import pydirectinput
+import speech_recognition
+import pyttsx3
 
 def main():
-    for i in range(5, 0, -1):
-        print(i)
-        time.sleep(1)
     # ask for user input to which weapon they are using - mouse timings and other settings may vary with weapon
     # e.g. arthur is very slow, use separate combo timings
     print("Starting")
     commands.clickTopOption()
+    
+
+    recognizer = speech_recognition.Recognizer()
+    continueLoop = True
+    while continueLoop:
+        try:
+            with speech_recognition.Microphone() as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.01)
+                audio = recognizer.listen(mic)
+                text = recognizer.recognize_google(audio)
+                text = text.lower()
+                textArray = text.split(' ')
+                print("Recognized: " + text)
+                # classic for loop to check, as when user inputs, they probably
+                # want the first thing they say to be executed first
+                for word in textArray:
+                    if word == 'quit':
+                        continueLoop = False
+        except speech_recognition.UnknownValueError:
+            recognizer = speech_recognition.Recognizer()
+            continue
     print("Completed")
 
 if __name__ == "__main__":
